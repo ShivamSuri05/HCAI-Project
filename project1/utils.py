86bb6deb705b60c97cbc402b9b8eb54c30ace28b
Shivam Suri <shivamsuri0503@gmail.com>
2025-05-29 19:37:23 +0200

    added 3 supervised models, logistic, linear, random forest

--- project1/utils.py
+++ project1/utils.py
@@ -1,22 +1,27 @@
 import pandas as pd
 import numpy as np
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report, confusion_matrix, r2_score, mean_absolute_error, mean_squared_error
 
 def handle_missing(df, col, strategy):
-    if strategy == 'remove':
+    if strategy == 'drop':
         df = df[df[col].notnull()]
     elif strategy == 'mean':
         mean_val = df[col].mean()
-        df[col].fillna(mean_val, inplace=True)
+        df[col] = df[col].fillna(mean_val)
     elif strategy == 'median':
         median_val = df[col].median()
-        df[col].fillna(median_val, inplace=True)
+        df[col] = df[col].fillna(median_val)
     elif strategy == 'mode':
         mode_val = df[col].mode()
         if not mode_val.empty:
-            df[col].fillna(mode_val[0], inplace=True)
+            df[col] = df[col].fillna(mode_val[0])
     elif strategy == 'keep':
-        pass  # doing nothing
+        pass  # do nothing
     return df
+
+def round_decimal(value: float):
+    decimal_val = round(value, 4)
+    return decimal_val
 
 def handle_outliers(df, col, strategy):
     # Detect outliers using IQR method
@@ -32,5 +37,23 @@ def handle_outliers(df, col, strategy):
         df.loc[df[col] < lower_bound, col] = lower_bound
         df.loc[df[col] > upper_bound, col] = upper_bound
     elif strategy == 'keep':
-        pass  # doing nothing
+        pass  # do nothing
     return df
+
+def evaluate_classification(y_test, y_pred):
+    metrics = {
+        'accuracy': round_decimal(accuracy_score(y_test, y_pred)),
+        'precision_weighted': round_decimal(precision_score(y_test, y_pred, average='weighted', zero_division=0)),
+        'recall_weighted': round_decimal(recall_score(y_test, y_pred, average='weighted', zero_division=0)),
+        'f1_score_weighted': round_decimal(f1_score(y_test, y_pred, average='weighted', zero_division=0)),
+    }
+    return metrics
+
+def evaluate_regression(y_test, y_pred):
+    metrics = {
+        'r2_score': round_decimal(r2_score(y_test, y_pred)),
+        'mae': round_decimal(mean_absolute_error(y_test, y_pred)),
+        'mse': round_decimal(mean_squared_error(y_test, y_pred)),
+        'rmse': round_decimal(np.sqrt(mean_squared_error(y_test, y_pred)))
+    }
+    return metrics
