3c2ac144d6478e1be1ef5191b184c37f07024db2
Shivam Suri <shivamsuri0503@gmail.com>
2025-07-24 07:30:40 +0200

    added divs for frontend using dummy data

--- project4/views.py
+++ project4/views.py
@@ -6,6 +6,33 @@ def index(request):
 
 def download_pdf(request):
     raise Http404("Model file not found.")
-    
+
 def recommender(request):
-    return render(request, 'project4/recommender.html', {})
+    context = {}
+    if request.method == 'POST':
+        rating = float(request.POST.get('rating'))
+        movie_id = request.POST.get('movie_id')
+        print(f"User rated movie {movie_id} as {rating}")
+        # You could save the rating here if needed
+        context['rating_submitted'] = True
+        context['user_rating'] = rating
+
+    mode = request.GET.get('start', False)
+    
+    if mode or request.method == 'POST':
+        movie = {
+            'backdrop_url': 'https://image.tmdb.org/t/p/w780/ac0kRKTfiJ4GcoUfb0XIO5vgC8q.jpg',
+            'poster_url': 'https://image.tmdb.org/t/p/w500/jexoNYnPd6vVrmygwF6QZmWPFdu.jpg',
+            'name': 'Inception',
+            'genre': 'Action|Crime|Fantasy',
+            'summary': 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO.'
+        }
+        context = {
+            'start_system': True,
+            'movie': movie,
+            'rating_choices': [0.5 * i for i in range(10, 0, -1)],
+            'preview_msg': "If you rate this movie as 5, you may also like:<ul> <li>Inception (2010)</li><li>Nightmare on Elm Street, A (2010)</li><li>Master of Disguise, The (2002)</li></ul>"
+        }
+        return render(request, 'project4/recommender.html', context)
+    
+    return render(request, 'project4/recommender.html', context)
